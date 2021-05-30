865266d6d8e02505640f0356deee1857ed9ade90
gthecht <gdhecht@gmail.com>
2021-05-30 18:13:46 +0300

    Add parse time function and reslash to powershell client

--- src/powershellClient.py
+++ src/powershellClient.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
-import pandas as pd
+import dateutil.parser
+import os
 
 def runPowershell(cmd):
   return str(Popen(["powershell", cmd], stdout=PIPE).communicate() [0])
@@ -21,4 +22,29 @@ def getPSTable(cmd, items):
   tableList = [[cell.strip() for cell in row] for row in tableList]
   tableList = list(filter(lambda row: (row != items) and not (row[0].startswith("-")) and (row != [""] * len(items)), tableList))
   tableList.sort(key=lambda row: " ".join(row)) # sort the table alphebatically
+  tableList = reslash(tableList)
   return tableList
+
+def reslash(data_list, slash="\\"):
+  for ind, element in enumerate(data_list):
+    try:
+      if type(element) == list:
+        element = reslash(element, slash)
+        data_list[ind] = element
+      elif "\\" in element:
+        data_list[ind] = os.path.normpath(element)
+    except TypeError as err:
+      # value wasn't a string
+      continue
+  return data_list
+
+def parseTime(df, items):
+  for item in items:
+    try:
+        df[item] = df[item].map(lambda timeStr: \
+          dateutil.parser.parse(timeStr).isoformat())
+    except Exception as err:
+      print("Failed to parse start time field, with error: " + str(err))
+      raise err
+
+  return df
