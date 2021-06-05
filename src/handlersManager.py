19e9442cabf41dbca7e9faa9a675b07fd12c06f5
gthecht <gdhecht@gmail.com>
2021-06-05 22:08:25 +0300

    import projectsHandler to handlersManager

--- src/handlersManager.py
+++ src/handlersManager.py
@@ -2,12 +2,14 @@ import sys
 
 from apps.appsHandler import AppsHandler
 from files.filesHandler import FilesHandler
+from project.projectsHandler import ProjectsHandler
 
 class HandlersManager:
-  def __init__(self):
+  def __init__(self, paths):
     self.get_os()
     self.apps_handler = AppsHandler(self.os)
     self.files_handler = FilesHandler(self.os)
+    self.projects_handler = ProjectsHandler(paths, self.os)
 
   # Get operating system:
   def get_os(self):
@@ -16,11 +18,15 @@ class HandlersManager:
     else: raise TypeError("unknown system platform", sys.platform)
 
   def gather(self):
-    self.openApps = self.apps_handler.getOpenApps()
-    return self.openApps
+    project_paths = self.projects_handler.get_proj_paths()
+    project_time = self.projects_handler.get_proj_start_time()
+    self.open_files = self.files_handler.get_open_files(project_paths, project_time)
+    self.open_apps = self.apps_handler.getOpenApps()
+    return self.open_apps, self.open_files
 
 if __name__ == '__main__':
-  handlers_manager = HandlersManager()
+  import os
+  handlers_manager = HandlersManager([os.path.abspath('.')])
   data = handlers_manager.gather()
   print("DATA:")
   print(data)
