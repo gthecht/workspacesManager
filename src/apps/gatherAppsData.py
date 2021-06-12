402b76372e23063b7a6aeb494834d6e0d32c1b1a
gthecht <gdhecht@gmail.com>
2021-06-12 11:44:20 +0300

    Add underline to gatherAppsData functions

--- src/apps/gatherAppsData.py
+++ src/apps/gatherAppsData.py
@@ -10,56 +10,56 @@ sys.path.insert(1, parent)
 from handlersManager import HandlersManager
 
 class Gatherer:
-  def __init__(self, logDir):
-    self.logDir = logDir
-    self.handlersManager = HandlersManager([os.path.abspath('.')])
+  def __init__(self, log_dir):
+    self.log_dir = log_dir
+    self.handlers_manager = HandlersManager([os.path.abspath('.')])
     self.run = False
-    self.logFile = self.logDir + "log-" + datetime.now().strftime('%Y-%m-%dT%H-%M-%S') + ".csv"
+    self.log_file = self.log_dir + "log-" + datetime.now().strftime('%Y-%m-%dT%H-%M-%S') + ".csv"
     self.SLEEP_TIME = 30 # time to sleep between data collection - note that gathering the apps takes about 2 seconds
 
   def gather(self):
     self.run = True
     try:
-      self.handlersManager.gather()
-      self.handlersManager.save()
-      self.data = self.handlersManager.get_apps()
+      self.handlers_manager.gather()
+      self.handlers_manager.save()
+      self.data = self.handlers_manager.get_apps()
     except Exception as err:
       print("Failed to gather data with err: " + str(err))
       print("Waiting a bit, and trying to gather again")
       sleep(self.SLEEP_TIME)
       return self.gather()
 
-    self.data.to_csv(self.logFile, index=False)
+    self.data.to_csv(self.log_file, index=False)
     while self.run:
       sleep(self.SLEEP_TIME)
       print("Gathering data")
       try:
-        self.handlersManager.gather()
-        self.handlersManager.save()
-        data = self.handlersManager.get_apps()
+        self.handlers_manager.gather()
+        self.handlers_manager.save()
+        data = self.handlers_manager.get_apps()
       except Exception as err:
         print("Failed to gather data with err: " + str(err))
         print("Waiting a bit, and trying to gather again")
         continue
       else:
-        self.compareData(data)
-        self.data.to_csv(self.logFile, index=False)
+        self.compare_data(data)
+        self.data.to_csv(self.log_file, index=False)
 
-  def compareData(self, data):
+  def compare_data(self, data):
     # find rows from the old that are duplicated in the new:
-    concatDf = pd.concat([data, self.data], ignore_index=True)
-    duplicated = concatDf.duplicated(keep='first')
+    concat_df = pd.concat([data, self.data], ignore_index=True)
+    duplicated = concat_df.duplicated(keep='first')
     ended = [not dup for dup in duplicated[len(data):]] # these are the old apps that have ended
     now = datetime.now().isoformat()[:-7]
     for ind in [i for i, e in enumerate(ended) if e]:
-      if concatDf.loc[ind + len(data.index)]["EndTime"] == "":
-        concatDf.loc[ind + len(data.index)]["EndTime"] = now
+      if concat_df.loc[ind + len(data.index)]["EndTime"] == "":
+        concat_df.loc[ind + len(data.index)]["EndTime"] = now
 
-    duplicatedstart = concatDf.duplicated(keep='last')
-    started = [not dup for dup in duplicatedstart[:len(data)]] # these are the new apps that have started
+    duplicated_start = concat_df.duplicated(keep='last')
+    started = [not dup for dup in duplicated_start[:len(data)]] # these are the new apps that have started
     for ind in [i for i, e in enumerate(started) if e]:
-      concatDf.loc[ind]["StartTime"] = now
-    self.data = concatDf.drop_duplicates().sort_values(["App", "Name", "StartTime", "EndTime"], ignore_index=True)
+      concat_df.loc[ind]["StartTime"] = now
+    self.data = concat_df.drop_duplicates().sort_values(["App", "Name", "StartTime", "EndTime"], ignore_index=True)
 
   def stop(self):
     self.run = False
