1c3beda92603d7f1b532ce99f3c04f667195bf3c
gthecht <gdhecht@gmail.com>
2021-06-12 12:16:47 +0300

    Turn filesHandler to async

--- src/files/filesHandler.py
+++ src/files/filesHandler.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 from datetime import datetime
+import asyncio
 
 parent = os.path.abspath('./src')
 sys.path.insert(1, parent)
@@ -25,36 +26,35 @@ class FilesHandler:
     print("files handler constructed")
 
   # Get the childitems of the given project
-  def get_open_files(self, paths, start_time):
+  async def get_open_files(self, paths, start_time):
     if paths is None: return pd.DataFrame()
     if type(start_time) == datetime:
       start_time = start_time.isoformat()
     if self.os == "windows":
-      open_files = self.get_files_from_powershell(paths, start_time)
+      open_files = await self.get_files_from_powershell(paths, start_time)
       return open_files
     else: return unknown_OS_Warning()
 
-  def get_files_from_powershell(self, paths, start_time):
+  async def get_files_from_powershell(self, paths, start_time):
     child_items = []
     for path in paths:
       cmd = "Get-ChildItem " + path + \
             " -Recurse -ErrorAction silentlycontinue " + \
             "| Where-Object { $_.LastAccessTime -gt \"" + \
             start_time + "\"}"
-      new_child_items = PSClient.get_PS_table(cmd, self.items)
+      new_child_items = await PSClient.get_PS_table(cmd, self.items)
       for child_item in new_child_items:
         child_items.append(child_item)
     open_files = pd.DataFrame(child_items, columns=self.items)
     # parse time fields:
-    open_files = PSClient.parse_time(
+    open_files = await PSClient.parse_time(
       open_files, ["LastWriteTime", "LastAccessTime"]
     )
     return open_files
 
 if __name__ == '__main__':
   now = datetime.now().isoformat()[:10]
-  proj = Project([os.path.abspath('.')], now)
   files_handler = FilesHandler()
-  open_files = files_handler.get_open_files(proj)
+  open_files = asyncio.run(files_handler.get_open_files([os.path.abspath('.')], now))
   print("open files:")
   print(open_files)
