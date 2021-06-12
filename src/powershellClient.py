706afab9dc9d68a92c0680db0f49045a70fc7951
gthecht <gdhecht@gmail.com>
2021-06-12 11:57:01 +0300

    Turn PS client to async

--- src/powershellClient.py
+++ src/powershellClient.py
@@ -2,19 +2,20 @@ from subprocess import Popen, PIPE
 import dateutil.parser
 import os
 
-def run_powershell(cmd):
+async def run_powershell(cmd):
   return str(Popen(["powershell", cmd], stdout=PIPE).communicate() [0])
 
-def pipe_PS_to_List(cmd):
-  out_str = run_powershell(cmd)
+async def pipe_PS_to_List(cmd):
+  out_str = await run_powershell(cmd)
   out = out_str.split("\\r\\n")
   out = out[2:len(out) - 3]
   out = [row.strip() for row in out]
   return out
 
-def get_PS_table(cmd, items):
+async def get_PS_table(cmd, items):
   full_cmd = cmd + " | Format-Table " + ", ".join(items) + " -AutoSize | Out-String -Width 1024"
-  rows_list = run_powershell(full_cmd).split("\\r\\n")[1:-2]
+  ps_out = await run_powershell(full_cmd)
+  rows_list = ps_out.split("\\r\\n")[1:-2]
   if len(rows_list) == 0: return []
   places = [rows_list[0].lower().find(item.lower()) for item in items]
   if len(items) == 1: table_list = [[row] for row in rows_list]
@@ -22,14 +23,14 @@ def get_PS_table(cmd, items):
   table_list = [[cell.strip() for cell in row] for row in table_list]
   table_list = list(filter(lambda row: (row != items) and not (row[0].startswith("-")) and (row != [""] * len(items)), table_list))
   table_list.sort(key=lambda row: " ".join(row)) # sort the table alphebatically
-  table_list = reslash(table_list)
+  table_list = await reslash(table_list)
   return table_list
 
-def reslash(data_list, slash="\\"):
+async def reslash(data_list, slash="\\"):
   for ind, element in enumerate(data_list):
     try:
       if type(element) == list:
-        element = reslash(element, slash)
+        element = await reslash(element, slash)
         data_list[ind] = element
       elif "\\" in element:
         data_list[ind] = os.path.normpath(element)
@@ -38,7 +39,7 @@ def reslash(data_list, slash="\\"):
       continue
   return data_list
 
-def parse_time(df, items):
+async def parse_time(df, items):
   for item in items:
     try:
         df[item] = df[item].map(lambda timeStr: \
