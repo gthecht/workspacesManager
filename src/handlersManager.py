510ef45606f23404faaffcebf00c13d852fd7740
gthecht <gdhecht@gmail.com>
2021-06-12 16:31:20 +0300

    Turn HandlersManager class to async

--- src/handlersManager.py
+++ src/handlersManager.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import sys
+import asyncio
 
 from apps.appsHandler import AppsHandler
 from files.filesHandler import FilesHandler
@@ -18,18 +19,26 @@ class HandlersManager:
     elif "linux" in sys.platform: self.os = "linux"
     else: raise TypeError("unknown system platform", sys.platform)
 
-  def gather(self):
-    print("  open files...")
-    project_paths = self.projects_handler.get_proj_paths()
-    project_time = self.projects_handler.get_proj_start_time()
-    self.open_files = self.files_handler.get_open_files(project_paths, project_time)
-    print("  open apps...")
-    self.open_apps = self.apps_handler.get_open_apps()
+  async def init(self):
+    await self.apps_handler.get_apps()
+
+  async def gather(self):
+    await asyncio.gather(self.gather_files(), self.gather_apps())
     self.projects_handler.update(self.open_files, self.open_apps)
     return
 
-  def save(self):
-    self.projects_handler.save()
+  async def gather_files(self):
+    print("  open files...")
+    project_paths = self.projects_handler.get_proj_paths()
+    project_time = self.projects_handler.get_proj_start_time()
+    self.open_files = await self.files_handler.get_open_files(project_paths, project_time)
+
+  async def gather_apps(self):
+    print("  open apps...")
+    self.open_apps = await self.apps_handler.get_open_apps()
+
+  async def save(self):
+    await self.projects_handler.save()
 
   def get_apps(self):
     return self.open_apps
@@ -39,14 +48,15 @@ if __name__ == '__main__':
   import time
   t0 = time.perf_counter()
   manager = HandlersManager([os.path.abspath('.')])
+  asyncio.run(manager.init())
   manager.projects_handler.set_current(name="workspacesManager")
   print("initialization took: ", time.perf_counter() - t0, "seconds")
   time.sleep(10)
   print("Gathering:")
   t1 = time.perf_counter()
-  manager.gather()
+  asyncio.run(manager.gather())
   print("Gathering took: ", time.perf_counter() - t1, "seconds")
   t2 = time.perf_counter()
-  manager.save()
-  print("Gathering took: ", time.perf_counter() - t2, "seconds")
+  asyncio.run(manager.save())
+  print("Saving took: ", time.perf_counter() - t2, "seconds")
 
