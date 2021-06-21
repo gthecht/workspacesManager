8bef531b148f53f983018018ac03a996527a1015
gthecht <gdhecht@gmail.com>
2021-06-21 23:02:58 +0300

    Change handlersManager to manager

---
+++ src/manager.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import asyncio
+
+project = os.path.abspath('./src/project')
+sys.path.insert(1, project)
+from projectsHandler import ProjectsHandler
+
+gatherer = os.path.abspath('./src/gatherer')
+sys.path.insert(1, gatherer)
+from gatherer import Gatherer
+
+class Manager:
+  def __init__(self, paths, log_dir):
+    self.get_os()
+    self.log_dir = log_dir
+
+    self.projects_handler = ProjectsHandler(paths, self.os)
+    self.gatherer = Gatherer(self.log_dir,self.projects_handler, self.os)
+
+  # Get operating system:
+  def get_os(self):
+    if "win" in sys.platform: self.os = "windows"
+    elif "linux" in sys.platform: self.os = "linux"
+    else: raise TypeError("unknown system platform", sys.platform)
+
+  async def init(self):
+    await self.gatherer.init()
+
+  async def gather(self):
+    await self.gatherer.gather()
+    return
+
+  async def save(self):
+    await self.gatherer.save()
+
+  def get_apps(self):
+    return self.gatherer.open_apps
+
+if __name__ == '__main__':
+  import time
+  t0 = time.perf_counter()
+  log_dir = "C:/Users/GiladHecht/Workspace/workspacesManager/logs/"
+  manager = Manager([os.path.abspath('.')], log_dir)
+  asyncio.run(manager.init())
+  manager.projects_handler.set_current(name="workspacesManager")
+  print("initialization took: ", time.perf_counter() - t0, "seconds")
+  time.sleep(10)
+  print("Gathering:")
+  t1 = time.perf_counter()
+  asyncio.run(manager.gather())
+  print("Gathering took: ", time.perf_counter() - t1, "seconds")
+  t2 = time.perf_counter()
+  asyncio.run(manager.save())
+  print("Saving took: ", time.perf_counter() - t2, "seconds")
+
