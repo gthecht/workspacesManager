244e2b9a9f6eac039bdb8ec9cbb993338716f36d
gthecht <gdhecht@gmail.com>
2021-07-17 15:44:30 +0300

    Turn Gatherer into a thread class

--- src/gatherer/gatherer.py
+++ src/gatherer/gatherer.py
@@ -1,45 +1,75 @@
 from time import sleep
 from datetime import datetime
 import pandas as pd
+import threading
+import queue
 
 from appsGatherer import AppsGatherer
 from filesGatherer import FilesGatherer
 
-class Gatherer:
+class Gatherer(threading.Thread):
   def __init__(self, log_dir, projects_handler, os="windows"):
     self.log_dir = log_dir
     self.os = os
     self.apps_handler = AppsGatherer(self.os)
     self.files_handler = FilesGatherer(self.os)
     self.projects_handler = projects_handler
+    self.reply_q = queue.Queue()
 
-    self.run = False
+    self.running = False
     self.log_file = self.log_dir + "log-" + datetime.now().strftime('%Y-%m-%dT%H-%M-%S') + ".csv"
     self.SLEEP_TIME = 30 # time to sleep between data collection - note that gathering the apps takes about 2 seconds
+    super().__init__(name="gatherer", daemon=True)
 
-  def gather(self):
-    self.run = True
+  def add_job(self, job):
+    self.projects_handler.q.put(job)
+    if "reply_q" not in job.keys(): return
+    else:
+      reply = None
+      while self.reply_q.empty():
+        continue
+      reply = self.reply_q.get()
+      return reply
+
+  def run(self):
+    self.running = True
     try:
       self.gather_files()
       self.gather_apps()
-      self.projects_handler.update(self.open_files, self.open_apps)
-      self.projects_handler.save()
+      # self.projects_handler.update(self.open_files, self.open_apps)
+      job = {
+        "method": "update",
+        "args": {
+          "open_files": self.open_files,
+          "open_apps": self.open_apps
+        },
+      }
+      self.add_job(job)
+
       self.data = self.open_apps
     except Exception as err:
       print("Failed to gather data with err: " + str(err))
       print("Waiting a bit, and trying to gather again")
       sleep(self.SLEEP_TIME)
-      return self.gather()
+      return self.run()
 
     self.data.to_csv(self.log_file, index=False)
-    while self.run:
+    while self.running:
       sleep(self.SLEEP_TIME)
       print("Gathering data")
       try:
         self.gather_files()
         self.gather_apps()
-        self.projects_handler.update(self.open_files, self.open_apps)
-        self.projects_handler.save()
+        # self.projects_handler.update(self.open_files, self.open_apps)
+        job = {
+          "method": "update",
+          "args": {
+            "open_files": self.open_files,
+            "open_apps": self.open_apps
+          },
+        }
+        self.add_job(job)
+
         data = self.open_apps
       except Exception as err:
         print("Failed to gather data with err: " + str(err))
@@ -52,7 +82,19 @@ class Gatherer:
   def gather_files(self):
     print("  open files...")
     project_paths = self.projects_handler.get_proj_paths()
+    proj_paths_job = {
+      "method": "get_proj_paths",
+      "args": {},
+      "reply_q": self.reply_q
+    }
+    self.add_job(proj_paths_job)
     project_time = self.projects_handler.get_proj_start_time()
+    proj_time_job = {
+      "method": "get_proj_start_time",
+      "args": {},
+      "reply_q": self.reply_q
+    }
+    self.add_job(proj_time_job)
     self.open_files = self.files_handler.get_open_files(project_paths, project_time)
 
   def gather_apps(self):
@@ -76,7 +118,7 @@ class Gatherer:
     self.data = concat_df.drop_duplicates().sort_values(["App", "Name", "StartTime", "EndTime"], ignore_index=True)
 
   def stop(self):
-    self.run = False
+    self.running = False
 
 if __name__ == '__main__':
   import os
@@ -87,4 +129,6 @@ if __name__ == '__main__':
 
   projects_handler = ProjectsHandler([os.path.abspath('.')], "windows")
   gatherer = Gatherer("C:/Users/GiladHecht/Workspace/workspacesManager/logs/", projects_handler)
-  gatherer.gather()
+  projects_handler.start()
+  gatherer.start()
+  gatherer.join()
