98e34b3f4ca326a8578dba814728e64b196c54ab
gthecht <gdhecht@gmail.com>
2021-11-13 23:38:03 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="rarian",
-  version='0.0.1',
+  version='0.1.0',
   author="Gilad Hecht",
   description='workspace manager to help you manage your work',
   extras_require={
