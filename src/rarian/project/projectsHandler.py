0fbb619462d61aa059d1dce59aca20e26205f671
gthecht <gdhecht@gmail.com>
2022-02-05 21:20:51 +0200

    Format Project and ProjectsHandler

--- src/rarian/project/projectsHandler.py
+++ src/rarian/project/projectsHandler.py
@@ -1,3 +1,4 @@
+from rarian.project.project import Project
 import sys
 import os
 import json
@@ -6,152 +7,170 @@ import queue
 
 parent = os.path.abspath('./src')
 sys.path.insert(1, parent)
-from rarian.project.project import Project
+
 
 class ProjectsHandler(threading.Thread):
-  def __init__(self, data_path, os="windows"):
-    self.os = os
-    self.data_path = data_path
-    self.load_data()
-    self.projects = {}
-    self.init_projects()
-    self.running = True
-    self.q = queue.Queue()
-    super().__init__(name="projectHandler", daemon=True)
+    def __init__(self, data_path, os="windows"):
+        self.os = os
+        self.data_path = data_path
+        self.load_data()
+        self.projects = {}
+        self.init_projects()
+        self.running = True
+        self.q = queue.Queue()
+        super().__init__(name="projectHandler", daemon=True)
 
-  def run(self):
-    while self.running:
-      job = self.q.get()
-      method = getattr(self, job["method"])
-      value = method(**job["args"])
-      # return the value if its supposed to be returned:
-      if "reply_q" in job.keys(): job["reply_q"].put(value)
-      self.q.task_done()
+    def run(self):
+        while self.running:
+            job = self.q.get()
+            method = getattr(self, job["method"])
+            value = method(**job["args"])
+            # return the value if its supposed to be returned:
+            if "reply_q" in job.keys():
+                job["reply_q"].put(value)
+            self.q.task_done()
 
-  def load_data(self):
-    with open(self.data_path, 'r') as data_file:
-      data_str = data_file.read()
-    data = json.loads(data_str)
-    self.projects_paths = data["projects"]
-    self.current = None
-    self.default_author = data["default author"]
+    def load_data(self):
+        with open(self.data_path, 'r') as data_file:
+            data_str = data_file.read()
+        data = json.loads(data_str)
+        self.projects_paths = data["projects"]
+        self.current = None
+        self.default_author = data["default author"]
 
-  def init_projects(self):
-    for ind, path in enumerate(self.projects_paths):
-      try:
-        path = os.path.normpath(path)
-        project = Project.load(path)
-      except FileNotFoundError:
-        print("Error: path '" + path + "' is not  a rarian project")
-        del self.projects_paths[ind]
-        continue
-      if project.name in self.projects.keys():
-        print("Error: duplicate project name -", project.name)
-        continue
-      self.projects[project.name] = project
+    def init_projects(self):
+        for ind, path in enumerate(self.projects_paths):
+            try:
+                path = os.path.normpath(path)
+                project = Project.load(path)
+            except FileNotFoundError:
+                print("Error: path '" + path + "' is not  a rarian project")
+                del self.projects_paths[ind]
+                continue
+            if project.name in self.projects.keys():
+                print("Error: duplicate project name -", project.name)
+                continue
+            self.projects[project.name] = project
 
-  # Open and close:
-  def set_current(self, name=None, path=None):
-    if (not name) and (not path): return
-    if (not name) and path:
-      path = os.path.normpath(path)
-      for proj_name in self.projects:
-        if path == self.projects[proj_name].path(): name = proj_name
+    # Open and close:
+    def set_current(self, name=None, path=None):
+        if (not name) and (not path):
+            return
+        if (not name) and path:
+            path = os.path.normpath(path)
+            for proj_name in self.projects:
+                if path == self.projects[proj_name].path():
+                    name = proj_name
 
-    if name not in self.projects.keys():
-      raise ValueError("Project doesn't exist")
-    # Turn previous project off:
-    if name == self.current: return
-    if self.current: self.projects[self.current].turn_off()
-    self.current = name
-    # Turn new project on:
-    self.projects[self.current].turn_on()
-    print(self.current, "is now the current project")
+        if name not in self.projects.keys():
+            raise ValueError("Project doesn't exist")
+        # Turn previous project off:
+        if name == self.current:
+            return
+        if self.current:
+            self.projects[self.current].turn_off()
+        self.current = name
+        # Turn new project on:
+        self.projects[self.current].turn_on()
+        print(self.current, "is now the current project")
 
-  def get_current(self):
-    return self.current
+    def get_current(self):
+        return self.current
 
-  def save(self):
-    data_dict = {
-      "projects": self.projects_paths,
-      "current": self.current,
-      "default author": self.default_author,
-    }
-    with open(self.data_path, 'w') as data_path:
-      json.dump(data_dict, data_path, sort_keys=True, indent=2)
-    if self.current is None: return None
-    self.projects[self.current].save()
+    def save(self):
+        data_dict = {
+            "projects": self.projects_paths,
+            "current": self.current,
+            "default author": self.default_author,
+        }
+        with open(self.data_path, 'w') as data_path:
+            json.dump(data_dict, data_path, sort_keys=True, indent=2)
+        if self.current is None:
+            return None
+        self.projects[self.current].save()
 
-  def close_project(self):
-    if self.current:
-      self.projects[self.current].turn_off()
-      self.current = None
+    def close_project(self):
+        if self.current:
+            self.projects[self.current].turn_off()
+            self.current = None
 
-  # Insert:
-  def new_project(
-    self,
-    paths,
-    name=None,
-    proj_type=None,
-    author=None,
-    start_time=None,
-    dirs=None,
-    files=None,
-    apps=[]
-  ):
-    """Create new project"""
-    if name in self.projects.keys():
-      raise ValueError(f"project of name {name} already exists")
-    else:
-      project = Project(paths, name, proj_type, author, start_time, dirs, files, apps)
-      self.projects[project.name] = project
-      self.projects_paths.append(paths[0])
+    # Insert:
+    def new_project(
+        self,
+        paths,
+        name=None,
+        proj_type=None,
+        author=None,
+        start_time=None,
+        dirs=None,
+        files=None,
+        apps=[]
+    ):
+        if name in self.projects.keys():
+            raise ValueError(f"project of name {name} already exists")
+        else:
+            project = Project(paths, name, proj_type, author,
+                              start_time, dirs, files, apps)
+            self.projects[project.name] = project
+            self.projects_paths.append(paths[0])
 
-  def remove_sub_dir(self, path):
-    """Removes sub directories, for instance when a sub directory is a project"""
-    if self.current: self.projects[self.current].remove_sub_dir(path)
-    else: print("Assign project before you remove a directory")
+    def remove_sub_dir(self, path):
+        if self.current:
+            self.projects[self.current].remove_sub_dir(path)
+        else:
+            print("Assign project before you remove a directory")
 
-  def add_sub_dir(self, path):
-    """Adds a sub directory - that possibly was removed"""
-    raise NotImplementedError
+    def add_sub_dir(self, path):
+        """Adds a sub directory - that possibly was removed"""
+        raise NotImplementedError
 
-  # Gather:
-  def update(self, open_files):
-    if self.current is None: return None
-    self.projects[self.current].update(open_files)
-    self.save()
+    # Gather:
+    def update(self, open_files):
+        if self.current is None:
+            return None
+        self.projects[self.current].update(open_files)
+        self.save()
 
-  # Get:
-  def get_open(self):
-    """Get the open project, files, apps and more"""
-    if self.current is None: return None
-    return self.projects[self.current].get_open()
+    # Get:
+    def get_open(self):
+        """Get the open project, files, apps and more"""
+        if self.current is None:
+            return None
+        return self.projects[self.current].get_open()
 
-  def get_proj_dirs(self):
-    if self.current is None: return None
-    return self.projects[self.current].dirs
+    def get_proj_dirs(self):
+        if self.current is None:
+            return None
+        return self.projects[self.current].dirs
 
-  def get_proj_start_time(self):
-    if self.current is None: return None
-    return self.projects[self.current].start_time
+    def get_proj_start_time(self):
+        if self.current is None:
+            return None
+        return self.projects[self.current].start_time
 
-  def get_project_data(self, member, sort_by="Relevance"):
-    if member == "projects": return list(self.projects.keys())
-    if self.current is None: return None
-    if member == "files":
-      data = self.projects[self.current].files
-      data.sort_values(by=sort_by, ascending=False, inplace=True)
-    elif member == "apps": data = self.projects[self.current].apps
-    elif member == "notes": data = self.projects[self.current].notes
-    elif member == "urls": data = self.projects[self.current].urls
-    else: raise ValueError("undefined data member")
-    return data
+    def get_project_data(self, member, sort_by="Relevance"):
+        if member == "projects":
+            return list(self.projects.keys())
+        if self.current is None:
+            return None
+        if member == "files":
+            data = self.projects[self.current].files
+            data.sort_values(by=sort_by, ascending=False, inplace=True)
+        elif member == "apps":
+            data = self.projects[self.current].apps
+        elif member == "notes":
+            data = self.projects[self.current].notes
+        elif member == "urls":
+            data = self.projects[self.current].urls
+        else:
+            raise ValueError("undefined data member")
+        return data
 
-  def opened_file(self, file, app):
-    if self.current is None: return None
-    return self.projects[self.current].update_opened_file(file, app)
+    def opened_file(self, file, app):
+        if self.current is None:
+            return None
+        return self.projects[self.current].update_opened_file(file, app)
 
-  def stop(self):
-    self.save()
-    self.running = False
+    def stop(self):
+        self.save()
+        self.running = False
--- test/src/project/conftest.py
+++ test/src/project/conftest.py
@@ -8,107 +8,125 @@ from rarian.project.projectsHandler import ProjectsHandler
 
 # constants:
 file_items = [
-  "Name",
-  "LastWriteTime",
-  "LastAccessTime",
-  "Extension",
-  "Directory",
-  "FullName"
+    "Name",
+    "LastWriteTime",
+    "LastAccessTime",
+    "Extension",
+    "Directory",
+    "FullName"
 ]
 test_proj = os.path.abspath("./test/testProject")
 tmp_proj = os.path.abspath("./test/tmp Project")
 empty_proj = os.path.abspath("./test/emptyProject")
 proj_name = "test project"
 project_dict = {
-  "paths": [empty_proj],
-  "name": "TestProj",
-  "proj_type": "test",
-  "author": "pytest",
+    "paths": [empty_proj],
+    "name": "TestProj",
+    "proj_type": "test",
+    "author": "pytest",
 }
 
+
 def init_project():
-  if os.path.exists(empty_proj): shutil.rmtree(empty_proj)
-  shutil.copytree(test_proj, empty_proj)
-  shutil.rmtree(os.path.join(empty_proj, ".rarian"))
-  proj_from_dict = Project(**project_dict)
-  if os.path.exists(empty_proj): shutil.rmtree(empty_proj)
-  return proj_from_dict
+    if os.path.exists(empty_proj):
+        shutil.rmtree(empty_proj)
+    shutil.copytree(test_proj, empty_proj)
+    shutil.rmtree(os.path.join(empty_proj, ".rarian"))
+    proj_from_dict = Project(**project_dict)
+    if os.path.exists(empty_proj):
+        shutil.rmtree(empty_proj)
+    return proj_from_dict
+
 
 proj_from_dict = init_project()
 
 open_files = pd.DataFrame()
 
 # fixtures:
+
+
 @pytest.fixture
 def test_proj_path():
-  return tmp_proj
+    return tmp_proj
+
 
 @pytest.fixture
 def empty_proj_path():
-  return empty_proj
+    return empty_proj
+
 
 @pytest.fixture
 def project_details():
-  return project_dict
+    return project_dict
+
 
 @pytest.fixture
 def project_name():
-  return proj_name
+    return proj_name
+
 
 @pytest.fixture
 def create_uninitialized_project():
-  if os.path.exists(empty_proj): shutil.rmtree(empty_proj)
-  shutil.copytree(test_proj, empty_proj)
-  shutil.rmtree(os.path.join(empty_proj, ".rarian"))
-  yield
-  if os.path.exists(empty_proj): shutil.rmtree(empty_proj)
+    if os.path.exists(empty_proj):
+        shutil.rmtree(empty_proj)
+    shutil.copytree(test_proj, empty_proj)
+    shutil.rmtree(os.path.join(empty_proj, ".rarian"))
+    yield
+    if os.path.exists(empty_proj):
+        shutil.rmtree(empty_proj)
+
 
 @pytest.fixture
 def create_initialized_project():
-  shutil.copytree(test_proj, tmp_proj)
-  yield
-  shutil.rmtree(tmp_proj)
+    shutil.copytree(test_proj, tmp_proj)
+    yield
+    shutil.rmtree(tmp_proj)
+
 
 @pytest.fixture
 def load_project():
-  shutil.copytree(test_proj, tmp_proj)
-  yield Project.load(tmp_proj)
-  shutil.rmtree(tmp_proj)
+    shutil.copytree(test_proj, tmp_proj)
+    yield Project.load(tmp_proj)
+    shutil.rmtree(tmp_proj)
+
 
 @pytest.fixture
 def init_new_project():
-  return proj_from_dict
+    return proj_from_dict
+
 
 @pytest.fixture
 def add_new_file():
-  def _add_new_file(dir_path):
-    tmp_dir = os.path.join(dir_path, "temp")
-    os.mkdir(tmp_dir)
-    new_file_path = os.path.join(tmp_dir, "tmp.txt")
-    with open (new_file_path, 'w') as file:
-      file.write("temporary file")
-    return new_file_path
-  return _add_new_file
+    def _add_new_file(dir_path):
+        tmp_dir = os.path.join(dir_path, "temp")
+        os.mkdir(tmp_dir)
+        new_file_path = os.path.join(tmp_dir, "tmp.txt")
+        with open(new_file_path, 'w') as file:
+            file.write("temporary file")
+        return new_file_path
+    return _add_new_file
 
 
-#%% ProjectsHandler
+# %% ProjectsHandler
 app_data_dict = {
-  "projects": [tmp_proj],
-  "default author": "pytest",
-  "current": None,
+    "projects": [tmp_proj],
+    "default author": "pytest",
+    "current": None,
 }
 
 app_data_path = os.path.abspath("./test/appData//data.json")
 
+
 @pytest.fixture
 def project_handler_file():
-  return app_data_path
+    return app_data_path
+
 
 @pytest.fixture
 def create_project_handler():
-  shutil.copytree(test_proj, tmp_proj)
-  proj_handler = ProjectsHandler(app_data_path)
-  yield proj_handler
-  shutil.rmtree(tmp_proj)
-  with open(app_data_path, 'w') as data_file:
-      json.dump(app_data_dict, data_file, sort_keys=True, indent=2)
+    shutil.copytree(test_proj, tmp_proj)
+    proj_handler = ProjectsHandler(app_data_path)
+    yield proj_handler
+    shutil.rmtree(tmp_proj)
+    with open(app_data_path, 'w') as data_file:
+        json.dump(app_data_dict, data_file, sort_keys=True, indent=2)
