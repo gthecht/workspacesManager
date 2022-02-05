5a7a61c6074d1a062befece3e23e6f7f38e9ae38
gthecht <gdhecht@gmail.com>
2022-02-05 21:17:31 +0200

    Format main and unknownOsWarning

--- src/rarian/main.py
+++ src/rarian/main.py
@@ -2,6 +2,6 @@ import os
 from rarian.manager import Manager
 
 if __name__ == '__main__':
-  data_dir = os.getenv('APPDATA') + "\\Rarian"
-  manager = Manager(data_dir)
-  manager.run()
+    data_dir = os.getenv('APPDATA') + "\\Rarian"
+    manager = Manager(data_dir)
+    manager.run()
--- src/rarian/unknownOSWarning.py
+++ src/rarian/unknownOSWarning.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def unknown_OS_Warning():
     # need to make it work for linux
     raise TypeError("unknown system platform", sys.platform)
